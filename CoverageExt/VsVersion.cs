e31f7b00b7a7aa852a5430cc9e49a9c96a32cbe6
Michał Antoniak <m.antoniak@posnet.com.pl>
2024-07-19 12:13:40 +0200

    Line coverage coloring has been improved for Visual Studio 2022 or (probably) later. The change makes the colored lines in Visual Studio consistent with what is in the report.

---
+++ CoverageExt/VsVersion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace NubiloSoft.CoverageExt
+{
+    public static class VsVersion
+    {
+        static readonly object mLock = new object();
+        static Version mVsVersion;
+
+        public static Version FullVersion
+        {
+            get
+            {
+                lock (mLock)
+                {
+                    if (mVsVersion == null)
+                    {
+                        string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "msenv.dll");
+
+                        if (File.Exists(path))
+                        {
+                            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(path);
+
+                            string verName = fvi.ProductVersion;
+
+                            for (int i = 0; i < verName.Length; i++)
+                            {
+                                if (!char.IsDigit(verName, i) && verName[i] != '.')
+                                {
+                                    verName = verName.Substring(0, i);
+                                    break;
+                                }
+                            }
+                            mVsVersion = new Version(verName);
+                        } else
+                            mVsVersion = new Version(0, 0); // Not running inside Visual Studio!
+                    }
+                }
+
+                return mVsVersion;
+            }
+        }
+
+        public static bool Vs2022OrLater
+        {
+            get { return FullVersion >= new Version(17, 0); }
+        }
+    }
+}
