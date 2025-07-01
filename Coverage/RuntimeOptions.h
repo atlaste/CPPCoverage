1b8c81e664ce6fba85720a3e30b1ced0acd5746e
Michał Antoniak <m.antoniak@posnet.com.pl>
2025-07-01 10:22:06 +0200

    Skip copying an empty string to an empty string

--- Coverage/RuntimeOptions.h
+++ Coverage/RuntimeOptions.h
@@ -42,33 +42,26 @@ public:
 
 	std::string SourcePath()
 	{
-		if (sourcePath.empty())
+		if (sourcePath.empty() && !CodePath.empty())
 		{
-			if (!CodePath.empty())
+			auto idx = CodePath.find("x64");
+			if (idx == std::string::npos)
 			{
-				auto idx = CodePath.find("x64");
-				if (idx == std::string::npos)
-				{
-					idx = CodePath.find("Debug");
-				}
-				if (idx == std::string::npos)
-				{
-					idx = CodePath.find("Release");
-				}
-				if (idx == std::string::npos)
-				{
-					idx = CodePath.find('\\');
-				}
-				if (idx == std::string::npos)
-				{
-					throw "Cannot locate source file base for this executable";
-				}
-				sourcePath = CodePath.substr(0, idx);
+				idx = CodePath.find("Debug");
 			}
-			else
+			if (idx == std::string::npos)
 			{
-				sourcePath = CodePath;
+				idx = CodePath.find("Release");
 			}
+			if (idx == std::string::npos)
+			{
+				idx = CodePath.find('\\');
+			}
+			if (idx == std::string::npos)
+			{
+				throw "Cannot locate source file base for this executable";
+			}
+			sourcePath = CodePath.substr(0, idx);
 		}
 		return sourcePath;
 	}
