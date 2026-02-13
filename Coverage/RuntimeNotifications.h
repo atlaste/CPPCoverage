d3faaad3355bbf27e1775e512b179f7c1190b3c7
Michał Antoniak <m.antoniak@posnet.com.pl>
2026-02-13 09:32:42 +0100

    RuntimeNotifications: privatisation of methods and field

--- Coverage/RuntimeNotifications.h
+++ Coverage/RuntimeNotifications.h
@@ -11,11 +11,13 @@ struct RuntimeCoverageFilter
 
 struct RuntimeNotifications
 {
+public:
+  void Handle(const char* data, const size_t size);
+  bool IgnoreFile(const std::string& filename) const;
+private:
   std::vector<std::unique_ptr<RuntimeCoverageFilter>> postProcessing;
 
   std::string Trim(const std::string& t);
   std::string GetFQN(std::string s);
   void PrintInvalidNotification(const std::string& notification, const std::string_view information);
-  void Handle(const char* data, const size_t size);
-  bool IgnoreFile(const std::string& filename) const;
 };
