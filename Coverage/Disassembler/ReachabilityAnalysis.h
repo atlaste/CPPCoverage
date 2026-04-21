6b2eb3d5c81af1b97426ba96fdac95c3d0ed5a3b
Michał Antoniak <47522782+MAntoniak@users.noreply.github.com>
2026-04-21 18:37:53 +0200

    Reformat files using editconfig.

--- Coverage/Disassembler/ReachabilityAnalysis.h
+++ Coverage/Disassembler/ReachabilityAnalysis.h
@@ -7,60 +7,60 @@
 
 struct ReachabilityAnalysis
 {
-	ReachabilityAnalysis(const ReachabilityAnalysis&) = delete;
-	ReachabilityAnalysis& operator=(const ReachabilityAnalysis&) = delete;
+  ReachabilityAnalysis(const ReachabilityAnalysis&) = delete;
+  ReachabilityAnalysis& operator=(const ReachabilityAnalysis&) = delete;
 
-	ReachabilityAnalysis(ReachabilityAnalysis&& o) noexcept
-	{
-		state = 0;
-		methodStart = 0;
-		numberBytes = 0;
+  ReachabilityAnalysis(ReachabilityAnalysis&& o) noexcept
+  {
+    state = 0;
+    methodStart = 0;
+    numberBytes = 0;
 
-		std::swap(state, o.state);
-		std::swap(methodStart, o.methodStart);
-		std::swap(numberBytes, o.numberBytes);
-	}
+    std::swap(state, o.state);
+    std::swap(methodStart, o.methodStart);
+    std::swap(numberBytes, o.numberBytes);
+  }
 
-	ReachabilityAnalysis& operator=(ReachabilityAnalysis&& o) noexcept
-	{
-		if (this != &o)
-		{
-			delete state;
+  ReachabilityAnalysis& operator=(ReachabilityAnalysis&& o) noexcept
+  {
+    if (this != &o)
+    {
+      delete state;
 
-			state = 0;
-			methodStart = 0;
-			numberBytes = 0;
+      state = 0;
+      methodStart = 0;
+      numberBytes = 0;
 
-			std::swap(state, o.state);
-			std::swap(methodStart, o.methodStart);
-			std::swap(numberBytes, o.numberBytes);
-		}
-		return *this;
-	}
+      std::swap(state, o.state);
+      std::swap(methodStart, o.methodStart);
+      std::swap(numberBytes, o.numberBytes);
+    }
+    return *this;
+  }
 
-	ReachabilityAnalysis(HANDLE processHandle, DWORD64 methodStart, SIZE_T numberBytes);
+  ReachabilityAnalysis(HANDLE processHandle, DWORD64 methodStart, SIZE_T numberBytes);
 
-	static size_t FirstInstructionSize(HANDLE processHandle, DWORD64 methodStart);
+  static size_t FirstInstructionSize(HANDLE processHandle, DWORD64 methodStart);
 
-	DWORD64 methodStart;
-	size_t numberBytes;
-	uint8_t* state;
+  DWORD64 methodStart;
+  size_t numberBytes;
+  uint8_t* state;
 
-	bool operator<(const ReachabilityAnalysis& o) const
-	{
-		return methodStart < o.methodStart;
-	}
+  bool operator<(const ReachabilityAnalysis& o) const
+  {
+    return methodStart < o.methodStart;
+  }
 
-	friend void swap(ReachabilityAnalysis& lhs, ReachabilityAnalysis& rhs)
-	{
-		std::swap(lhs.state, rhs.state);
-		std::swap(lhs.methodStart, rhs.methodStart);
-		std::swap(lhs.numberBytes, rhs.numberBytes);
-	}
+  friend void swap(ReachabilityAnalysis& lhs, ReachabilityAnalysis& rhs)
+  {
+    std::swap(lhs.state, rhs.state);
+    std::swap(lhs.methodStart, rhs.methodStart);
+    std::swap(lhs.numberBytes, rhs.numberBytes);
+  }
 
-	~ReachabilityAnalysis()
-	{
-		delete[] state;
-		state = 0;
-	}
+  ~ReachabilityAnalysis()
+  {
+    delete[] state;
+    state = 0;
+  }
 };
